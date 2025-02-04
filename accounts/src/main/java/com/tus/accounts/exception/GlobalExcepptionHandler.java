400b8282ec19b2e1c992c406ab6d4b16f3e28088
Sagar Barate <sagarbarate2907@com.com>
2025-02-04 10:36:46 +0000

    Implementing a READ API that fetches the details based on mobile number

--- accounts/src/main/java/com/tus/accounts/exception/GlobalExcepptionHandler.java
+++ accounts/src/main/java/com/tus/accounts/exception/GlobalExcepptionHandler.java
@@ -7,7 +7,17 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.http.HttpStatus;
 import java.time.LocalDateTime;
 
-@controllerAdvice
+@ControllerAdvice
 public class GlobalExcepptionHandler {
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception, WebRequest webRequest){
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+            webRequest.getDescription(false),
+            HttpStatus.NOT_FOUND,
+            exception.getMessage(),
+            LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO,HttpStatus.NOT_FOUND);
+    }
 
 }
